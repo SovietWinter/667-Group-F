2e95cdeaccf2e72d3a73ff70d61384d22715c482
Jason Burmark <mr.burmark@gmail.com>
2014-11-23 23:18:12 -0800

    added follows so users may follow and be followed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 	has_many :posts
+
+	has_many :followings
+	has_many :follows, through: :followings
+	has_many :backwards_followings, class_name: "Following", foreign_key: "follow_id"
+	has_many :followers, through: :backwards_followings, source: :user
+	
+	
+	
 	validates_presence_of :username
 	validates_presence_of :email
 	validates_presence_of :real_name
