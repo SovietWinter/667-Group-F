53cd8ff9bb36d4dcc8bf68e2ce99f96d722fd939
Kirill <sovietwinter@gmail.com>
2014-11-25 04:53:33 -0800

    added basic user validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120205150) do
+ActiveRecord::Schema.define(version: 20141125045657) do
 
   create_table "posts", force: true do |t|
     t.string   "user_id"
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(version: 20141120205150) do
     t.string   "blog_name"
     t.string   "city"
     t.string   "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
